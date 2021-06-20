e3ce097b400cecef6957c34e6fb882ff2fdf39f0
František Žiačik <ziacik@gmail.com>
2021-06-20 22:33:53 +0200

    feat: low.initialize

---
+++ src/lib/low.spec.ts
@@ -0,0 +1,208 @@
+import {
+	init,
+	INPUT,
+	LOW,
+	mode,
+	msleep,
+	OUTPUT,
+	spiBegin,
+	spiChipSelect,
+	spiSetClockDivider,
+	spiSetCSPolarity,
+	spiSetDataMode,
+	spiWrite,
+	write,
+	read,
+} from "rpio";
+import { initialize, Pin } from "./low";
+
+jest.mock("rpio");
+
+describe("low level functions", () => {
+	let initOptions: Record<string, unknown>;
+	let modes: Record<Pin, number>;
+	let writes: string[];
+
+	beforeEach(() => {
+		modes = {} as Record<Pin, number>;
+		initOptions = {};
+		writes = [];
+		(init as jest.Mock).mockImplementation((options: Record<string, unknown>) => (initOptions = options));
+		(mode as jest.Mock).mockImplementation((pin: Pin, mode: number) => (modes[pin] = mode));
+		(write as jest.Mock).mockImplementation((pin: Pin, value: number) => writes.push(`${Pin[pin]}${value.toString(16)}`));
+		(msleep as jest.Mock).mockImplementation((delay: number) => writes.push(`~${delay}`));
+		(spiWrite as jest.Mock).mockImplementation((buffer: Buffer, len: number) => {
+			const values = Array(len)
+				.fill(0)
+				.map((_val, i) => buffer.readUInt8(i).toString(16));
+			writes.push(`:${values.join(",")}`);
+		});
+		(read as jest.Mock).mockReturnValue(0);
+	});
+
+	describe("initialize", () => {
+		it("initializes rpio with gpiomem = false and gpio mappings", () => {
+			initialize();
+			expect(initOptions).toEqual({
+				gpiomem: false,
+				mapping: "gpio",
+			});
+		});
+
+		it("sets up gpio pin modes", () => {
+			initialize();
+			expect(modes).toEqual({
+				[Pin.RST]: OUTPUT,
+				[Pin.CS]: OUTPUT,
+				[Pin.DC]: OUTPUT,
+				[Pin.BUSY]: INPUT,
+			});
+		});
+
+		it("sets up SPI", () => {
+			initialize();
+			expect(spiBegin).toHaveBeenCalledTimes(1);
+			expect(spiSetDataMode).toHaveBeenCalledWith(0);
+			expect(spiSetClockDivider).toHaveBeenCalledWith(128);
+			expect(spiChipSelect).toHaveBeenCalledWith(0);
+			expect(spiSetCSPolarity).toHaveBeenCalledWith(0, LOW);
+		});
+
+		it("writes all the commands and data needed for initialization", () => {
+			initialize();
+			expect(writes).toEqual([
+				"CS1",
+
+				"RST1",
+				"~200",
+				"RST0",
+				"~2",
+				"RST1",
+				"~200",
+
+				"DC0",
+				"CS0",
+				":12",
+				"CS1",
+				"~200",
+
+				"DC0",
+				"CS0",
+				":46",
+				"CS1",
+
+				"CS0",
+				"DC1",
+				":f7",
+				"CS1",
+				"~200",
+
+				"DC0",
+				"CS0",
+				":47",
+				"CS1",
+				"CS0",
+				"DC1",
+				":f7",
+				"CS1",
+				"~200",
+
+				"DC0",
+				"CS0",
+				":c",
+				"CS1",
+				"CS0",
+				"DC1",
+				":ae,c7,c3,c0,40",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":1",
+				"CS1",
+				"CS0",
+				"DC1",
+				":af,2,1",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":11",
+				"CS1",
+				"CS0",
+				"DC1",
+				":1",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":44",
+				"CS1",
+				"CS0",
+				"DC1",
+				":0,0,6f,3",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":45",
+				"CS1",
+				"CS0",
+				"DC1",
+				":af,2,0,0",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":3c",
+				"CS1",
+				"CS0",
+				"DC1",
+				":1",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":18",
+				"CS1",
+				"CS0",
+				"DC1",
+				":80",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":22",
+				"CS1",
+				"CS0",
+				"DC1",
+				":b1",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":20",
+				"CS1",
+				"~200",
+
+				"DC0",
+				"CS0",
+				":4e",
+				"CS1",
+				"CS0",
+				"DC1",
+				":0,0",
+				"CS1",
+
+				"DC0",
+				"CS0",
+				":4f",
+				"CS1",
+				"CS0",
+				"DC1",
+				":af,2",
+				"CS1",
+			]);
+		});
+	});
+});
---
+++ src/lib/low.ts
@@ -0,0 +1,105 @@
+import {
+	init,
+	INPUT,
+	LOW,
+	mode,
+	msleep,
+	OUTPUT,
+	read,
+	spiBegin,
+	spiChipSelect,
+	spiSetClockDivider,
+	spiSetCSPolarity,
+	spiSetDataMode,
+	spiWrite,
+	write,
+} from "rpio";
+
+export enum Pin {
+	RST = 17,
+	DC = 25,
+	CS = 8,
+	BUSY = 24,
+}
+
+export function initialize(): void {
+	init({
+		gpiomem: false,
+		mapping: "gpio",
+	});
+
+	mode(Pin.RST, OUTPUT);
+	mode(Pin.DC, OUTPUT);
+	mode(Pin.CS, OUTPUT);
+	mode(Pin.BUSY, INPUT);
+
+	write(Pin.CS, 1);
+
+	spiBegin();
+	spiSetDataMode(0);
+	spiSetClockDivider(128);
+	spiChipSelect(0);
+	spiSetCSPolarity(0, LOW);
+
+	reset();
+	command(0x12);
+	waitUntilIdle();
+	command(0x46);
+	data(0xf7);
+	waitUntilIdle();
+	command(0x47);
+	data(0xf7);
+	waitUntilIdle();
+	command(0x0c);
+	data(0xae, 0xc7, 0xc3, 0xc0, 0x40);
+	command(0x01);
+	data(0xaf, 0x02, 0x01);
+	command(0x11);
+	data(0x01);
+	command(0x44);
+	data(0x00, 0x00, 0x6f, 0x03);
+	command(0x45);
+	data(0xaf, 0x02, 0x00, 0x00);
+	command(0x3c);
+	data(0x01);
+	command(0x18);
+	data(0x80);
+	command(0x22);
+	data(0xb1);
+	command(0x20);
+	waitUntilIdle();
+	command(0x4e);
+	data(0x00, 0x00);
+	command(0x4f);
+	data(0xaf, 0x02);
+}
+
+function reset(): void {
+	write(Pin.RST, 1);
+	msleep(200);
+	write(Pin.RST, 0);
+	msleep(2);
+	write(Pin.RST, 1);
+	msleep(200);
+}
+
+function command(id: number) {
+	write(Pin.DC, 0);
+	write(Pin.CS, 0);
+	spiWrite(Buffer.alloc(1, id), 1);
+	write(Pin.CS, 1);
+}
+
+function data(...data: number[]) {
+	write(Pin.CS, 0);
+	write(Pin.DC, 1);
+	spiWrite(Buffer.from(data), data.length);
+	write(Pin.CS, 1);
+}
+
+function waitUntilIdle() {
+	while (read(Pin.BUSY)) {
+		msleep(10);
+	}
+	msleep(200);
+}

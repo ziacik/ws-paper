060277a0adb2c921b53cf3d5a1611bb631e8b2eb
František Žiačik <ziacik@gmail.com>
2021-08-04 20:30:11 +0200

    refactor: low tests

--- src/lib/low.spec.ts
+++ src/lib/low.spec.ts
@@ -21,23 +21,26 @@ jest.mock("rpio");
 describe("low level functions", () => {
 	let initOptions: Record<string, unknown>;
 	let modes: Record<Pin, number>;
-	let writes: string[];
+	let commands: string[];
 
 	beforeEach(() => {
 		modes = {} as Record<Pin, number>;
 		initOptions = {};
-		writes = [];
+		commands = [];
 		(init as jest.Mock).mockImplementation((options: Record<string, unknown>) => (initOptions = options));
 		(mode as jest.Mock).mockImplementation((pin: Pin, mode: number) => (modes[pin] = mode));
-		(write as jest.Mock).mockImplementation((pin: Pin, value: number) => writes.push(`${Pin[pin]}${value.toString(16)}`));
-		(msleep as jest.Mock).mockImplementation((delay: number) => writes.push(`~${delay}`));
+		(write as jest.Mock).mockImplementation((pin: Pin, value: number) => commands.push(`${Pin[pin]}${value.toString(16)}`));
+		(msleep as jest.Mock).mockImplementation((delay: number) => commands.push(`~${delay}`));
 		(spiWrite as jest.Mock).mockImplementation((buffer: Buffer, len: number) => {
 			const values = Array(len)
 				.fill(0)
 				.map((_val, i) => buffer.readUInt8(i).toString(16));
-			writes.push(`:${values.join(",")}`);
+			commands.push(`:${values.join(",")}`);
 		});
-		(read as jest.Mock).mockReturnValue(0);
+		(read as jest.Mock).mockImplementation((pin: Pin) => {
+			commands.push(`?${Pin[pin]}`);
+			return 0;
+		});
 	});
 
 	describe("initialize", () => {
@@ -70,7 +73,7 @@ describe("low level functions", () => {
 
 		it("writes all the commands and data needed for initialization", () => {
 			initialize();
-			expect(writes).toEqual([
+			expect(commands).toEqual([
 				"CS1",
 
 				"RST1",
@@ -84,6 +87,7 @@ describe("low level functions", () => {
 				"CS0",
 				":12",
 				"CS1",
+				"?BUSY",
 				"~200",
 
 				"DC0",
@@ -95,6 +99,7 @@ describe("low level functions", () => {
 				"DC1",
 				":f7",
 				"CS1",
+				"?BUSY",
 				"~200",
 
 				"DC0",
@@ -105,6 +110,7 @@ describe("low level functions", () => {
 				"DC1",
 				":f7",
 				"CS1",
+				"?BUSY",
 				"~200",
 
 				"DC0",
@@ -183,6 +189,7 @@ describe("low level functions", () => {
 				"CS0",
 				":20",
 				"CS1",
+				"?BUSY",
 				"~200",
 
 				"DC0",
